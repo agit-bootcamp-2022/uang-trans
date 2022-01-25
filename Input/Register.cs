20c9cc19597befb8cf404af6a8b6af0575309dd5
Dwi Cahyaningrum <dwicahyaningrum23@gmail.com>
2022-01-25 11:21:45 +0700

    feat: add record register

---
+++ Input/Register.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uang_trans.Input
+{
+    public record Register
+    (
+       string FullName,
+       string Email,
+       string Username,
+       string Password
+   );
+    
+}
