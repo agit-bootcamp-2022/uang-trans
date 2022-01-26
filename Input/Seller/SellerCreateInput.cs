538b14ac367fc1b966e92f4a1febcbf85399a58d
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-26 21:49:43 +0700

    fix input and add validation required

--- Input/Seller/SellerCreateInput.cs
+++ Input/Seller/SellerCreateInput.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace uang_trans.Input.Seller
 {
-    public record SellerCreateInput
-    (
-        int SellerId,
-        double AmountSeller
-    );
+    public class SellerCreateInput
+    {
+        [Required]
+        public int SellerId { get; set; }
+        [Required]
+        public double AmountSeller { get; set; }
+    }
 }
