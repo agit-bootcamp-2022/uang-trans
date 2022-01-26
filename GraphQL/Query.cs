7b55819b3ea6577fc0fb0c8891e51f7a49577a2c
ainaajidian <ainaajidian@gmail.com>
2022-01-26 17:59:03 +0700

    feat: Get All Roles

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -1,11 +1,18 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using HotChocolate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using uang_rans.Models;
+using uang_trans.Input.Role;
 using uang_trans.Models;
 
 namespace uang_trans.GraphQL
 {
     public class Query
     {
+
         public IQueryable<Customer> GetCustomersAsync([Service] AppDbContext context) =>
          context.Customers;
 
@@ -14,5 +21,18 @@ namespace uang_trans.GraphQL
 
         public IQueryable<Wallet> GetWalletsAsync([Service] AppDbContext context) =>
           context.Wallets;
+
+        //[Authorize(Roles = new [] {"Admin"})]
+        public List<Roles> GeRolesAsync([Service] AppDbContext context)
+        {
+            List<Roles> lstRole = new List<Roles>();
+            var results = context.Roles;
+            foreach (var role in results)
+            {
+                lstRole.Add(new Roles { Rolename = role.Name });
+            }
+
+            return lstRole;
+        }
     }
 }
