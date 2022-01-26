5794c6386647aa07e64c25bd8ef5dd9c861b29d9
Reno Satya Adrian <49367557+renosatyaadrian@users.noreply.github.com>
2022-01-26 12:39:17 +0700

    bugfix/user-topup (#12)
    * update entity
    
    * add http context service
    
    * fix configs
    
    * fix topup flow

---
+++ Migrations/20220126050238_UpdateWalletMutation.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace uang_trans.Migrations
+{
+    public partial class UpdateWalletMutation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Debit",
+                table: "WalletMutations",
+                newName: "Amount");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Balance",
+                table: "Wallets",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MutationType",
+                table: "WalletMutations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MutationType",
+                table: "WalletMutations");
+
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "WalletMutations",
+                newName: "Debit");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Balance",
+                table: "Wallets",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
