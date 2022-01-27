584de9a067c4477f89667c23097c3c7827be06fe
Suzev Atsma Alboris <Suzev.Alboris@atlantica.dev>
2022-01-27 10:49:26 +0700

    Update CustomerDto / ProfileOutput

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -51,11 +51,19 @@ namespace uang_trans.GraphQL
         }
 
         // [Authorize(Roles = new[] { "Admin","Customer" })]
-        public IQueryable<Customer> GetProfileByCustomerIdAsync([Service] AppDbContext context,
+        public IQueryable<ProfileOutput> GetProfileByCustomerIdAsync([Service] AppDbContext context,
                                                             [Service] IHttpContextAccessor httpContextAccessor)
         {
             var custId = Convert.ToInt32(httpContextAccessor.HttpContext.User.FindFirst("Id").Value);
-            return context.Customers.Where(p=> p.Id == custId);    
+            return context.Customers.Select(p => new ProfileOutput()
+            {
+                Id = p.Id,
+                Username = p.Username,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                Email = p.Email,
+                CreatedDate = p.CreatedDate
+            }).Where(x => x.Id == custId);            
         }   
     }
 }
